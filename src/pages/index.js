188436fda6cfb043c9fa0abcf7f8c14919d02445
Andrea R. Nigro <andrea.nigro@ogilvy.com>
2020-01-06 13:18:01 +0100

    aggiungi ingrediente alla lista della spesa

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,12 @@
 import React from "react"
 import SplashScreen from "../components/SplashScreen"
 
-export default () => <SplashScreen/>
+export default () => 
+    <>
+        <h1>Splash Screen</h1>
+        <ul>
+            <li>
+                <Link to="/app/">Vai all'app</Link>
+            </li>
+        </ul>
+    </>
