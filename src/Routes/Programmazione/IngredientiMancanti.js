7de800a7dd1b4482a164a4466d9758b87eb066e4
Andrea R. Nigro <andrea.nigro@ogilvy.com>
2020-01-06 10:51:09 +0100

    aggiungi ingrediente alla lista della spesa

--- src/Routes/Programmazione/IngredientiMancanti.js
+++ src/Routes/Programmazione/IngredientiMancanti.js
@@ -1,29 +1,67 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
+import { Dialog, DialogContent, Button, Grid } from '@material-ui/core';
+import { ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+
+const useStyles = makeStyles(theme => ({
+  ListItem: {
+      maxHeight: '65vh',
+      overflow: 'auto'
+  },
+  ul: {
+      padding: 0,
+  }
+}));
 
 export default ({ open, onClose, missing }) => {
+  const classes = useStyles()
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle id="simple-dialog-title">Ingredienti mancanti</DialogTitle>
-      {missing && Object.keys(missing).length 
-        ? 
-          <List>
-            {Object.keys(missing).map(ingredient => (
-              <ListItem key={ingredient}>
-                <ListItemText 
-                  primary={missing[ingredient].name} 
-                  secondary={`${missing[ingredient].qta} g`}
-                />
-              </ListItem>
-            ))}
-          </List>
-        : null 
-      }
+      <DialogContent>
+        <Grid container 
+            spacing={3}
+            justify="center"
+        >
+          <Grid item xs={12}>
+            {missing && Object.keys(missing).length 
+              ? 
+                <List className={classes.ListItem}>
+                  {Object.keys(missing).map(ingredient => (
+                    <ListItem key={ingredient}>
+                      <ListItemText 
+                        primary={missing[ingredient].name} 
+                        secondary={`${missing[ingredient].qta} g`}
+                      />
+                      <ListItemSecondaryAction>
+                        <IconButton edge="end">
+                          <ShoppingCart/>
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  ))}
+                </List>
+              : null 
+            }
+          </Grid>
+          <Grid item>
+            <Button 
+              variant="contained" 
+              color="primary"
+              onClick={onClose}
+            >
+              Chiudi
+            </Button>
+          </Grid>
+        </Grid>
+      </DialogContent>
     </Dialog>
   );
 }
